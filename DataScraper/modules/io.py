05d71c754f675f4ec61cd55451ddc045a08a7e8f
J-JUN_shim <wowns1537@gmail.com>
2021-03-14 18:37:49 +0900

    Create io.py

---
+++ DataScraper/modules/io.py
@@ -0,0 +1,58 @@
+import os
+import winreg
+import requests
+
+from io import BytesIO
+from PIL import Image
+
+
+def get_user_directory(extraDir=None):
+    directory = os.path.join(os.path.expanduser('~'), 'Downloads')
+
+    # if os.name == 'nt':
+    #     sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+    #     downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+
+    #     with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+    #         directory = winreg.QueryValueEx(key, downloads_guid)[0]
+
+    return os.path.join(directory, extraDir) if extraDir else directory
+
+
+def download_image(src, path):
+    response = requests.get(src)
+    img = Image.open(BytesIO(response.content)).convert('RGB')
+    img.save(f'{path}.jpg')
+
+
+def download_images(srcList, query, directory):
+    from datetime import datetime
+
+    def timestamp():
+        return datetime.now().strftime('%Y-%m-%d_%H%M%S')
+
+    directory = get_user_directory(
+        'images') if directory is None else os.path.abspath(directory)
+
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+
+    print(f"\n\nDownloading {len(srcList)} images to '{directory}'...")
+
+    total = 0
+    for i, src in enumerate(srcList, 1):
+        try:
+            filename = f'img{timestamp()}_{query}{i}'
+            download_image(src, f'{directory}{filename}')
+        except KeyboardInterrupt:
+            print('\nStop downloading!')
+            break
+        except:
+            total -= 1
+            print(f'failed: {filename}')
+            pass
+        else:
+            total += 1
+            print(f'success: {filename}')
+
+    print(f'\n{total if total>0 else 0}/{len(srcList)} files safely done')
